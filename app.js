386273f3337dcb210868aa94916ee24864b55aef
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-03 01:47:27 +0000

    typed out second half of file after var posts

--- app.js
+++ app.js
@@ -31,3 +31,24 @@ var posts = [
   }
 ]
 
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+
+//blog home page
+app.get('/post/:id', (req, res) => {
+  // find the post in the 'posts' array
+  var post = posts.filter((post) => {
+    return post.id == req.params.id
+  })[0]
+
+  //render the 'post.ejs' template with the post content
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body
+  })
+})
+
+app.listen(8080)
+
+console.log('listening on port 8080')
